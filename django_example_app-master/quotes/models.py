308a8ce034f914e9528d73d908e6225016cfaa7c
seleluciano <seleluciano4@gmail.com>
2023-10-10 20:49:35 -0300

    Migraciones

--- django_example_app-master/quotes/models.py
+++ django_example_app-master/quotes/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Sensor(models.Model):
-    ID = models.CharField(max_length=100)
     ubicacion = models.CharField(max_length=200)
     estado = models.BooleanField(default=False)
 
