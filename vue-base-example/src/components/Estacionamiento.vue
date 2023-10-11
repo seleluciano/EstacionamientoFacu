1a4384501b7817847207600c10c3db2de4f5fb9f
seleluciano <seleluciano4@gmail.com>
2023-10-10 21:19:25 -0300

    Vue

--- vue-base-example/src/components/Estacionamiento.vue
+++ vue-base-example/src/components/Estacionamiento.vue
@@ -1,21 +1,6 @@
-import axios from 'axios'
 <template>
     <div>
-        <label>Nombre</label><input v-model="name">
-        <label>Precio</label><input v-model="price">
-        <button @click="agregarItem()">Agregar</button>
-        <table>
-            <tr>
-                <th>Id</th>
-                <th>Nombre</th>
-                <th>Precio</th>
-            </tr>
-            <tr v-for="item in items" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.price }}</td>
-            </tr>
-        </table>
+        
     </div>
 </template>
 
@@ -27,39 +12,12 @@ export default {
     name: 'APITest',
     data: function () {
         return {
-            items: [],
-            name: '',
-            price: '',
+            
         }
     },
     methods: {
-        cargarDatos() {
-            axios.get('http://localhost:8000/items/')
-                .then(response => {
-                    this.items = response.data
-                    console.log(response.data)
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        },
-        agregarItem() {
-            axios.post('http://localhost:8000/items/', {
-                name: this.name,
-                price: this.price
-            })
-                .then(response => {
-                    console.log(response)
-                    this.cargarDatos()
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        }
-    },
-    mounted() {
-        this.cargarDatos()
-    }
+        
+    }  
 }
 </script>
 <style>
