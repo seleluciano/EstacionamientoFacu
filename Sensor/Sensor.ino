9d33a2a6a0748796884925b6cb51e5f161ccd95b
seleluciano <seleluciano4@gmail.com>
2023-10-19 17:47:53 -0300

    COnexion Arduino y django

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -1,9 +1,23 @@
-int OUT = 2;  //pin 2 Arduino connected to the sensor output
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+int OUT = 2;  // Pin 2 de Arduino conectado a la salida del sensor
 int D = 0, O = 0;
-bool guardarestado;
+bool guardarestado, estadoanterior = false; // Inicializa estadoanterior como falso
+
+const char* ssid = "TU_SSID";
+const char* password = "TU_CONTRASEÑA";
+const char* server = "DIRECCION_DEL_SERVIDOR";
+
 void setup() {
-  Serial.begin(9600);   //initiates serial monitor
-  pinMode(OUT, INPUT);  // setting pin 2 in Arduino as output
+  Serial.begin(9600);
+  pinMode(OUT, INPUT);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando a WiFi...");
+  }
+  Serial.println("Conectado a WiFi");
 }
 
 void loop() {
@@ -18,7 +32,6 @@ void loop() {
     delay(500);
   }
   if (D >= 5) {
-
     guardarestado = false;
     D = 0;
   } else {
@@ -27,4 +40,34 @@ void loop() {
   }
   Serial.print("Estado:");
   Serial.println(guardarestado);
+
+  if (estadoanterior != guardarestado) {
+    estadoanterior = guardarestado;
+    enviarEstado(guardarestado); // Llamamos a la función para enviar el estado
+  }
 }
+
+
+void enviarEstado(int sensor_id, bool estado) {
+  HTTPClient http;
+
+  String url = "http://localhost:8000/actualizar_estado/";
+  
+  url += "?id=";
+  url += sensor_id;
+  url += "&estado=";
+  url += (estado ? "1" : "0");
+
+  http.begin(url);
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println(payload);
+  } else {
+    Serial.println("Error en la petición HTTP");
+  }
+
+  http.end();
+}
+
